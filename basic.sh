9b6a0754f19977b0d478c83a5cc65e98b6921117
Joseal-u <67656858+Joseal-u@users.noreply.github.com>
2021-03-02 01:54:08 -0500

    Add files via upload

---
+++ basic.sh
@@ -0,0 +1,42 @@
+echo ""
+echo "			Thanks to Jejaka Tutorials"
+echo "             EHZ TUITORIALS "
+echo "				eDiTeD VeRsIoN"
+echo ""
+echo "
+╔═╗┬ ┬┌┐ ┌─┐┬─┐  ┬ ┬┌─┐┬ ┬┬┌─
+║  └┬┘├┴┐├┤ ├┬┘  ├─┤├─┤│││├┴┐
+╚═╝ ┴ └─┘└─┘┴└─  ┴ ┴┴ ┴└┴┘┴ ┴
+"
+echo ""
+
+echo "SIT BACK AND RELAX"
+echo ""
+echo ""
+
+
+echo "THIS WILL TAKE CARE OF EVERYTHING"
+echo ""
+echo ""
+
+echo "YOU WILL BE TOLD ONCE WHEN DONE"
+echo ""
+pkg update -y
+pkg upgrade -y
+pkg install python3 -y
+pip3 install bs4
+pip3 install rsa
+pip3 install pyaes
+pip3 install requests
+pip3 install asyncio
+pip3 install async_generator
+pip3 install colorama
+unzip ltc.zip
+rm -rf ltc.zip
+clear
+rm -rf basic.sh
+
+echo " DONE "
+echo "exiting..."
+
+echo "exit"
