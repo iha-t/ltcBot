08a41f56cd8e65f5050853a87b983515a145c79d
Joseal-u <67656858+Joseal-u@users.noreply.github.com>
2023-12-04 16:26:14 +0300

    Update basic.sh

--- basic.sh
+++ basic.sh
@@ -20,17 +20,8 @@ echo ""
 echo ""
 
 echo "YOU WILL BE TOLD ONCE WHEN DONE"
-echo ""
-pkg update -y
-pkg upgrade -y
-pip3 install bs4
-pip3 install rsa
-pip3 install pyaes
-pip3 install requests
-pip3 install asyncio
-pip3 install async_generator
-pip3 install colorama
-unzip ltc.zip
+echo "figure it out...."
+
 rm -rf ltc.zip
 clear
 rm -rf basic.sh
